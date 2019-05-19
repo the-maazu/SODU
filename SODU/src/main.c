297ea5f971bb842796e1c645f46a776f18822138
al-maaz <hassanmaazu@gmail.com>
2019-05-19 16:50:34 +0000

    single seat sensing

--- SODU/src/main.c
+++ SODU/src/main.c
@@ -29,9 +29,9 @@
  * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
  */
 #include <asf.h>
-#include "drivers/seat_driver.h"
 #include "drivers/port_driver.h"
 #include "string.h"
+#include "drivers/TC_driver.h"
 
 #define SEAT1 0
 #define SEAT2 1
@@ -39,67 +39,128 @@
 #define SEAT4 3
 
 #define SEAT1_PORT &PORTA
-#define SEAT2_PORT &PORTB
-#define SEAT3_PORT &PORTC
-#define SEAT4_PORT &PORTD
+#define SEAT2_PORT &PORTC
 
-#define SEAT1_TIMER &TCC0
-#define SEAT2_TIMER &TCD0
-#define SEAT3_TIMER &TCE0
-#define SEAT4_TIMER &TCF0
+#define STOP_CLOCK TCC0.CTRLA = ( TCC0.CTRLA & ~TC0_CLKSEL_gm ) | TC_CLKSEL_OFF_gc
+#define START_CLOCK TCC0.CTRLA = ( TCC0.CTRLA & ~TC0_CLKSEL_gm ) | TC_CLKSEL_DIV64_gc
+
+volatile uint8_t SEAT_MASK = 0x00;
+volatile bool sensing = false;
+
+void seat_port_init(PORT_t * port);
+void stimulate_seat(PORT_t * port);
 
 int main (void)
 {
 	/* Insert system clock initialization code here (sysclk_init()). */
-
+	
 	board_init();
 	
-	/* Setup all 4 seats*/
-	seat_init(SEAT1_PORT, SEAT1_TIMER);
-	//seat_init(SEAT2_PORT, SEAT2_TIMER);
-	//seat_init(SEAT3_PORT, SEAT3_TIMER);
-	//seat_init(SEAT4_PORT, SEAT4_TIMER);
+	/* Setup seats*/
+	seat_port_init(SEAT1_PORT);
+	//seat_port_init(SEAT2_PORT);
+	
+	/* set timer maximum value*/
+	TCC0.PER = 0xFFFF;
 	
 	gfx_mono_init();
 	gpio_set_pin_high(NHD_C12832A1Z_BACKLIGHT);
 			
 	/* Enable medium and high level interrupts in the PMIC. */
-	PMIC.CTRL |= (PMIC_MEDLVLEN_bm | PMIC_HILVLEN_bm) ;
+	PMIC.CTRL |= (PMIC_MEDLVLEN_bm | PMIC_HILVLEN_bm | PMIC_LOLVLEN_bm);
 
 	/* Enable the global interrupt flag. */
 	sei();
 	
-	stimulate_seat(SEAT1_PORT, SEAT1_TIMER);
-	//stimulate_seat(SEAT2_PORT, SEAT2_TIMER);
-	//stimulate_seat(SEAT3_PORT, SEAT3_TIMER);
-	//stimulate_seat(SEAT4_PORT, SEAT4_TIMER);
-	
 	while(true)
 	{
-		delay_us(1000000);
-		gfx_mono_draw_string( "Enter", 20, 8, &sysfont);
+		stimulate_seat(SEAT1_PORT);
+		while(sensing);
+		//stimulate_seat(SEAT2_PORT);
+		//while(sensing);	
 	}
 }
 
 ISR(PORTA_INT0_vect)
-{
-	check_seat(SEAT1, SEAT1_PORT, SEAT1_TIMER);
+{	
+	STOP_CLOCK;
+	
+	gpio_toggle_pin(LED1);
+	char string [9];
+	
+	if(PORTA.OUT & PIN0_bm)
+	{
+		START_CLOCK;
+		PORT_ClearPins(&PORTA, PIN0_bm);
+		return;
+	}
+	else
+	{
+		if(TCC0.CNT >= 13000)
+		SEAT_MASK |= (1 << 0);
+		else
+		SEAT_MASK &= ~(1 << 0);
+		
+		gfx_mono_draw_string("        ", 20, 8 , &sysfont);
+		gfx_mono_draw_string( utoa(SEAT_MASK, string, 2), 20, 8 , &sysfont);
+		
+		sensing = false;
+	}
 }
 
 //ISR(PORTB_INT0_vect)
 //{
-	//check_seat(SEAT2, SEAT2_PORT, SEAT2_TIMER);
-	//stimulate_seat(SEAT2_PORT, SEAT2_TIMER);
+	//STOP_CLOCK;
+	//
+	//gpio_toggle_pin(LED1);
+	//char string [9];
+	//
+	//if(PORTB.OUT & PIN0_bm)
+	//{
+		//START_CLOCK;
+		//PORT_ClearPins(&PORTB, PIN0_bm);
+		//return;
+	//}
+	//else
+	//{
+		//if(TCC0.CNT >= 13000)
+		//SEAT_MASK |= (1 << 0);
+		//else
+		//SEAT_MASK &= ~(1 << 0);
+		//
+		//gfx_mono_draw_string("        ", 20, 8 , &sysfont);
+		//gfx_mono_draw_string( utoa(TCC0.CNT, string, 10), 20, 8 , &sysfont);
+		//
+		//sensing = false;
+	//}
 //}
-//
-//ISR(PORTC_INT0_vect)
-//{
-	//check_seat(SEAT3, SEAT3_PORT, SEAT3_TIMER);
-	//stimulate_seat(SEAT3_PORT, SEAT3_TIMER);
-//}
-//
-//ISR(PORTD_INT0_vect)
-//{
-	//check_seat(SEAT4, SEAT4_PORT, SEAT4_TIMER);
-	//stimulate_seat(SEAT4_PORT, SEAT4_TIMER);
-//}
+
+
+void seat_port_init(PORT_t * port)
+{
+	PORT_SetPinAsOutput( port, PIN0_bp );
+	
+	PORT_SetPinsAsInput( port, PIN2_bm );
+	
+	/* Configure pin 2 as input, triggered on both edges. */
+	PORT_ConfigurePins( port,
+	PIN2_bm, // pin 2 supports full asynchronous sense
+	false,
+	false,
+	PORT_OPC_TOTEM_gc,
+	PORT_ISC_BOTHEDGES_gc);
+
+	
+	/* Configure Interrupt0 to have medium interrupt level, triggered by pin 2. */
+	PORT_ConfigureInterrupt0( port, PORT_INT0LVL_HI_gc, PIN2_bm );
+}
+
+void stimulate_seat(PORT_t * port)
+{
+	sensing = true;
+	
+	/* Clear CNT register*/
+	TCC0.CNT = 0x0000;
+	START_CLOCK;
+	PORT_SetPins(port, PIN0_bm);
+}
